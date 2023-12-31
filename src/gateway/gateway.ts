630b0334ac116957e75bc53b5576368fb57e8cc2
ZhVladyslav <133242329+ZhVladyslav@users.noreply.github.com>
2023-12-31 20:28:44 +0200

    add disconnect

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,11 +1,17 @@
 import { OnModuleInit } from '@nestjs/common';
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { table, updateTable } from 'src/constants/table';
 import { IUpdateTable } from 'src/types/table.type';
 
 @WebSocketGateway(3030, { cors: true })
-export class Gateway implements OnModuleInit {
+export class Gateway implements OnModuleInit, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
@@ -15,10 +21,14 @@ export class Gateway implements OnModuleInit {
 
   onModuleInit() {
     this.server.on('connection', (socket) => {
-      console.log(`Connected: ${socket.id}`);
+      console.log(`Connection: ${socket.id}`);
 
       socket.emit('tableInfo', table);
     });
+  }
+
+  handleDisconnect(client: any) {
+    console.log(`Disconnection: ${client.id}`);
   }
 
   @SubscribeMessage('updateTable')
